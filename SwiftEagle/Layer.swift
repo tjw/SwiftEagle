9a9fcb4c5b84d651d37c83e10975862cc42087c9
Timothy J. Wood <tjw@omnigroup.com>
2015-08-14 13:31:17 -0700

    Added a few more layers.

--- SwiftEagle/Layer.swift
+++ SwiftEagle/Layer.swift
@@ -12,4 +12,12 @@ public enum Layer: UInt {
     case Top = 1
     case Bottom = 16
     case Dimension = 20
+    
+    case TopStopMask = 29
+    case BottomStopMask = 30
+    
+    case TopKeepout = 39
+    case BottomKeepout = 40
+    case TopRestrict = 41
+    case BottomRestrict = 42
 }
