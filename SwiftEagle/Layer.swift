9a4e948c8920f42aeeddd443960884b5f268c499
Timothy J. Wood <tjw@omnigroup.com>
2015-08-14 19:36:42 -0700

    More layer enum elements.

--- SwiftEagle/Layer.swift
+++ SwiftEagle/Layer.swift
@@ -13,6 +13,15 @@ public enum Layer: UInt {
     case Bottom = 16
     case Dimension = 20
     
+    case TopPlace = 21
+    case BottomPlace = 22
+
+    case TopOrigins = 23
+    case BottomOrigins = 24
+    
+    case TopNames = 25
+    case BottomNames = 26
+    
     case TopStopMask = 29
     case BottomStopMask = 30
     
@@ -20,4 +29,7 @@ public enum Layer: UInt {
     case BottomKeepout = 40
     case TopRestrict = 41
     case BottomRestrict = 42
+    
+    case TopDocumentation = 51
+    case BottomDocumentation = 52
 }
